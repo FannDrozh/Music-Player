f9ecc99e9b3157598f7dfc6a95fab5edf86ec43d
unknown <@ngknn.local>
2022-05-25 12:23:14 +0300

    Main forms v1

---
+++ Music Player/Entre.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Music_Player
+{
+    /// <summary>
+    /// Логика взаимодействия для Window2.xaml
+    /// </summary>
+    public partial class Entre : Window
+    {
+        public Entre()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+
+        private void Entree_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NotAc_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            //Entre entre = new Entre();
+            //entre.Close();
+            Registration registration = new Registration();
+            registration.Show();
+            this.Hide();
+            
+        }
+    }
+}
---
+++ Music Player/Registration.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Music_Player
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Window
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void Image_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Registration registration = new Registration();
+            registration.Close();
+            Entre entre = new Entre();
+            entre.Show();
+            this.Hide();
+            
+        }
+    }
+}
